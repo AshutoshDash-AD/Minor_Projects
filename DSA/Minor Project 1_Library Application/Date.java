0541a113d9974383f4d089a171fab7831f1c297a
Ashutosh Dash <53340217+AshutoshDash-AD@users.noreply.github.com>
2021-12-25 01:39:46 +0530

    Add files via upload

---
+++ DSA/Minor Project 1_Library Application/Date.java
@@ -0,0 +1,20 @@
+class Date {
+	int dd;
+	int mm;
+	int yyyy;
+	Date(int a,int b,int c){
+		dd=a;
+		mm=b;
+		yyyy=c;
+	}
+	public int getDD(){
+		return dd;
+	}
+	public int getMM(){
+		return mm;
+	}
+	public int getYYYY(){
+		return yyyy;	
+	}
+    
+}
